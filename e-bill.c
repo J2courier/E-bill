6356fd60fd8fc4c929102f021a1d5da7ef63da15
J2 <149977907+J2courier@users.noreply.github.com>
2024-02-29 22:44:51 +0800

    Update bugs and fix + formula e-bill.c

--- e-bill.c
+++ e-bill.c
@@ -12,10 +12,11 @@ void gotoxy(int x,int y){
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-int main(){
+int main (){
     float previous_reading , present_reading, present_counter, previous_counter;
-    float kwh_used_1, kwh_used_2, kwh_used, total_bill;
+    float kwh1, kwh2, kwh, total_bill;
     float past_bill, present_bill, kwh_per_hrs = 0.5;
+    float prev, pres;
     char ans;
     do{
         system("cls");
@@ -27,94 +28,95 @@ int main(){
         g(59, 2);s("%f", &previous_reading);
         g(19, 4);s("%f", &present_counter);
         g(59, 4);s("%f", &previous_counter);
+        //1.1
+        if (present_reading == previous_reading){
+            if (present_counter == previous_counter){
+                kwh1 = 0;
+                kwh2 = 0;
+                total_bill = 50;
+                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh2);
+                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
+            }//1.2
+            else if (present_counter > previous_counter){
+                prev = previous_counter * 1000 + previous_reading;
+                pres = present_counter * 1000 + present_reading;
+                kwh1 = pres - prev;
+                total_bill = 50;
+                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh1);
+                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
+            }//1.3
+            else if (present_counter < previous_counter){
+                prev = previous_counter * 1000 + previous_reading;
+                pres = present_counter * 1000 + present_reading;
+                kwh1 = 100000 - (prev + pres);
+                total_bill = 50;
+                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh1);
+                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
+            }
+        }
+        //2.1
+        if (present_reading > previous_reading){
+            if (present_counter == previous_counter){
+                prev = previous_counter * 1000 + previous_reading;
+                pres = present_counter * 1000 + present_reading;
+                kwh1 = pres - prev;
+                total_bill = 50;
+                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh1);
+                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
+            }
+            else if(present_counter > previous_counter){
+                prev = previous_counter * 1000 + previous_reading;
+                pres = present_counter * 1000 + present_reading;
+                kwh1 = pres - prev;
+                total_bill = kwh1 * 0.50;
+                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh1);
+                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
+            }
+            if (previous_counter > present_counter){
+                prev = previous_counter * 1000 + previous_reading;
+                pres = present_counter * 1000 + present_reading;
+                kwh1 = 100000 - (previous_counter * 1000) + pres;
+                total_bill = kwh1 * 0.50;
+                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh1);
+                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
+            }
+        }
+        //3.1
+        if (present_reading < previous_reading){
+            if (present_counter == previous_counter){
+                prev = previous_counter * 1000 + previous_reading;
+                pres = present_counter * 1000 + present_reading;
+                kwh1 = 100000 - prev;
+                total_bill = kwh1 * 0.50;
+                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh1);
+                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
+            }
+            else if (present_counter > previous_counter){
+                prev = previous_counter * 1000 + previous_reading;
+                pres = present_counter * 1000 + present_reading;
+                kwh1 = pres - prev;
+                total_bill = kwh1 * 0.50;
+                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh1);
+                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
+            }
+            else if (present_counter < previous_counter){
+                prev = previous_counter * 1000 + previous_reading;
+                pres = present_counter * 1000 + present_reading;
+                kwh1 = 100000 - prev;
+                total_bill = kwh1 * 0.50;
+                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh1);
+                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
+            }
+        }
 
-        //this is the condition for [prev == pres]
-        if (previous_reading == present_reading){
-            if (previous_counter > present_counter){
-                kwh_used_1 = (100 - previous_counter + present_counter - 1);
-                kwh_used_2 = 1000 - (kwh_used_1 * 1000); //1000 - (kwh_used_1 x 1000)
-                total_bill = kwh_per_hrs * kwh_used_2;
-                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh_used_2);
-                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
-            }
-            else if (previous_counter == present_counter){
-                kwh_used_1 = 0;
-                kwh_used_2 = 0;
-                total_bill = kwh_per_hrs * kwh_used_2;
-                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh_used_2);
-                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
-            }
-            else if (previous_counter < present_counter){
-                kwh_used_1 = (100 - present_counter + previous_counter - 1);
-                kwh_used_2 = (kwh_used_1 * 1000) - 1000;
-                total_bill = kwh_per_hrs * kwh_used_2;
-                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh_used_2);
-                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
-            }
-        }
-        //this is the condition for [prev > pres]
-        if (previous_reading > present_reading){
-            if (previous_counter > present_counter){
-                kwh_used_1 = (100 - previous_counter + present_counter - 1);
-                kwh_used_2 = (1000 - previous_reading + present_reading)+(kwh_used_1 * 1000);
-                total_bill = kwh_per_hrs * kwh_used_2;
-                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh_used_2);
-                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
-            }
-            else if (previous_counter == present_counter){
-                kwh_used_1 = (100 - previous_counter + present_counter - 1);
-                kwh_used_2 = (1000 - previous_reading + present_reading)+(kwh_used_1 * 1000);
-                total_bill = kwh_per_hrs * kwh_used_2;
-                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh_used_2);
-                g(2, 7);p("Electric Bill is: %0.2f", total_bill);;
-            }
-            else if (previous_counter < present_counter){
-                kwh_used_1 = (100 - present_counter + previous_counter - 1);
-                kwh_used_2 = (1000 - previous_reading + present_reading)+(kwh_used_1 * 1000);
-                total_bill = kwh_per_hrs * kwh_used_2;
-                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh_used_2);
-                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
-            }
-        }
-        //this is the condition for [pres > prev]
-        if (previous_reading < present_reading){
-            if (previous_counter < present_counter){
-                kwh_used_1 = 100 - previous_counter + present_counter - 1;
-                kwh_used_2 = (1000 - previous_reading + present_reading) + 100;
-                total_bill = kwh_per_hrs * kwh_used_2;
-                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh_used_2);
-                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
-            }
-            if (previous_counter > present_counter){
-                kwh_used_1 = 100 - previous_counter + present_counter - 1;
-                kwh_used_2 = (1000 - previous_reading + present_reading) + (kwh_used_1 * 1000);
-                total_bill = kwh_per_hrs * kwh_used_2;
-                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh_used_2);
-                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
-            } 
-            if (previous_counter == present_counter){
-                kwh_used_1 = 100 - previous_counter + present_counter - 1;
-                kwh_used_2 = (1000 - previous_reading + present_reading) + (kwh_used_1 * 1000);
-                total_bill = kwh_per_hrs * kwh_used_2;
-                g(2, 6);p("Kilowatt-hour used is: %0.2f", kwh_used_2);
-                g(2, 7);p("Electric Bill is: %0.2f", total_bill);
-            }
-        }
-        
         g(2, 8);p("Another Customer? Y/N: ");
         g(25, 8);s("%s", &ans);
-         g(19, 2);p("                  ");
+        g(19, 2);p("                   ");
         g(59, 2);p("                   ");
         g(19, 4);p("                   ");
-        g(59 , 4);p("                   ");
-        g(20, 6);p("                     ");
-        g(20, 7);p("                    ");
-        g(20, 8);p("                    ");
-    } while (ans == 'Y' || ans == 'y');  
-    system("cls");
+        g(59 , 4);p("                  ");
+        g(20, 6);p("                   ");
+        g(20, 7);p("                   ");
+        g(20, 8);p("                   ");
+    } while (ans == 'Y' || ans == 'y');
 }
-
-//kwhused_1 = (previous_reading - present_reading) + (present_counter * 1000) - (previous_counter * 1000);
-//previous = (previous_counter * 1000 + previous_reading);
-//present = (present_counter * 1000 + present_reading);
-//kwhused = (previous - 100000 + present);
